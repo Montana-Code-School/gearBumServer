8559b10679077baaf896909c8151704c63d5bb6d
Soren Thompson <sorenct04@gmail.com>
2016-08-02 15:57:12 -0600

    added error handling to equip routes

--- controllers/equipController.js
+++ controllers/equipController.js
@@ -1,42 +1,62 @@
 function createEquip (req, res){
 	console.log('Create Equip')
 	req.db.query(`INSERT INTO gb.gb_equip VALUES ('${req.body.category}', '7/15/16', 'luke', '${req.body.price}', '${req.body.description}', '${req.body.imageAddress}', '${req.body.title}', 30, 45, False)`, function(err, sqlRes){
-		res.json(sqlRes)
+		if (err) {
+			res.status(500).json(Object.assign(err, {sqlError: true});
+		} else {
+			res.json(sqlRes)
+		}
 	})
 }
 
 function deleteEquip (req, res){
 	req.db.query(`DELETE FROM gb.gb_equip WHERE gb.gb_equip.equipid = '${req.body.equipid}'`, function(err, sqlRes){
-		res.json(sqlRes)
+		if (err) {
+			res.status(500).json(Object.assign(err, {sqlError: true});
+		} else {
+			res.json(sqlRes)
+		}
 	})
 }
 
 function updateEquip (req, res){
 	//UPDATE table SET columnn name = new value WHERE column = old value
 	req.db.query("UPDATE gb.gb_equip SET category = 'bike' WHERE category = 'boat'", function(err, sqlRes){
-		res.json(sqlRes)
+		if (err) {
+			res.status(500).json(Object.assign(err, {sqlError: true});
+		} else {
+			res.json(sqlRes)
+		}
 	})
 }
 
 function retrieveEquipByCat (req, res){
 	req.db.query(`select * from gb.gb_equip WHERE category = '${req.params.category}'`, function(err, sqlRes){
-      res.json(sqlRes.rows)
-    })
+      	if (err) {
+			res.status(500).json(Object.assign(err, {sqlError: true});
+		} else {
+			res.json(sqlRes.rows)
+		}    
+	})
 }
 
 function retrieveAllEquip (req, res){
 	req.db.query(`select * from gb.gb_equip`, function(err, sqlRes){
-      res.json(sqlRes.rows)
+        if (err) {
+			res.status(500).json(Object.assign(err, {sqlError: true});
+		} else {
+			res.json(sqlRes.rows)
+		} 
     })
 }
 
 function retrieveEquipById (req, res){
 	req.db.query(`select * from gb.gb_equip WHERE equipid = '${req.params.equipid}'`, function(err, sqlRes){
-	  if(err){
-	  	console.error(err)
-	  	return res.json({err: err})
-	  }
-      res.json(sqlRes.rows)
+	    if (err) {
+			res.status(500).json(Object.assign(err, {sqlError: true});
+		} else {
+			res.json(sqlRes.rows)
+		} 
     })
 }
 
