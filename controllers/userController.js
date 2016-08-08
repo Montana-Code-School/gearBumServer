f5a71d2c31f63094dffaf7ab9d6cdf8abaee0fef
Soren Thompson <sorenct04@gmail.com>
2016-08-08 11:00:02 -0600

    deleted unnecessary commented out code

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,16 +27,7 @@ function updateUserByEmail (req, res){
 		}    
 	})
 }
-//UPDATE table SET columnn name = new value WHERE column = old value
-	// req.db.query("UPDATE gb.gb_equip SET category = 'bike' WHERE category = 'boat'", function(err, sqlRes){
-	// 	if (err) {
-	// 		res.json(Object.assign(err, {sqlError: true}));
-	// 	} else {
-	// 		res.json(sqlRes)
-	// 	}
-	// })
 
-// var User = require("../models/userModel")
 
 function login (req, res){
 	return res.json({message: "to be implemented"})
